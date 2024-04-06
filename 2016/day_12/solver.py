f39cd520d5e8c387cd6775f112c136b491c831e6
EffePi <francesco.penone@gmail.com>
2024-04-06 16:52:08 +0200

    Optimized day12, AoC 2016

--- 2016/day_12/solver.py
+++ 2016/day_12/solver.py
@@ -1,5 +1,3 @@
-import re
-
 
 def parse_input():
     with open("input.txt") as f:
@@ -7,74 +5,59 @@ def parse_input():
     instructions = input_txt.splitlines()
     return instructions
 
+
+def get_register_or_int(val, registers):
+    if val in registers:
+        val = registers[val]
+    else:
+        val = int(val)
+    return val
+
+
+def apply_instruction(idx, registers, instructions):
+    instr = instructions[idx]
+    instr_type, instr_args_txt = instr.split(" ", 1)
+    instr_args = instr_args_txt.split()
+
+    idx += 1
+    if instr_type == 'cpy':
+        val, reg_id = instr_args
+        val = get_register_or_int(val, registers)
+        registers[reg_id] = val
+    elif instr_type == 'inc':
+        reg_id, = instr_args
+        registers[reg_id] += 1
+    elif instr_type == 'dec':
+        reg_id, = instr_args
+        registers[reg_id] -= 1
+    elif instr_type == 'jnz':
+        val, jump_len = instr_args
+        val = get_register_or_int(val, registers)
+        jump_len = get_register_or_int(jump_len, registers)
+        if val != 0:
+            idx += int(jump_len) - 1
+    return idx, registers
+
+
 # PART 1
 registers = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
 instructions = parse_input()
+toggled = [False for _ in instructions]
 
 idx = 0
 while idx < len(instructions):
-    instr = instructions[idx]
-    idx += 1
-    if instr.startswith('cpy'):
-        (val, reg_id), = re.findall(r"cpy (.+) (.+)", instr)
-        if val in registers:
-            registers[reg_id] = registers[val]
-        else:
-            registers[reg_id] = int(val)
-    elif instr.startswith('inc'):
-        (reg_id), = re.findall(r"inc (.+)", instr)
-        registers[reg_id] += 1
-    elif instr.startswith('dec'):
-        (reg_id), = re.findall(r"dec (.+)", instr)
-        registers[reg_id] -= 1
-    elif instr.startswith('inc'):
-        (reg_id), = re.findall(r"inc (.+)", instr)
-        registers[reg_id] += 1
-    elif instr.startswith('jnz'):
-        (val, jump_len), = re.findall(r"jnz (.*) (.*)", instr)
-        try:
-            val = int(val)
-        except:
-            val = registers[val]
-        if val != 0:
-            idx += int(jump_len) - 1
-
+    idx, registers = apply_instruction(idx, registers=registers, instructions=instructions)
 print(f"Part 1 solution: {registers['a']}")
 
 
 # PART 2
 registers = {'a': 0, 'b': 0, 'c': 1, 'd': 0}
-instructions = parse_input()
 
 idx = 0
 while idx < len(instructions):
-    instr = instructions[idx]
-    idx += 1
-    if instr.startswith('cpy'):
-        (val, reg_id), = re.findall(r"cpy (.+) (.+)", instr)
-        if val in registers:
-            registers[reg_id] = registers[val]
-        else:
-            registers[reg_id] = int(val)
-    elif instr.startswith('inc'):
-        (reg_id), = re.findall(r"inc (.+)", instr)
-        registers[reg_id] += 1
-    elif instr.startswith('dec'):
-        (reg_id), = re.findall(r"dec (.+)", instr)
-        registers[reg_id] -= 1
-    elif instr.startswith('jnz'):
-        (val, jump_len), = re.findall(r"jnz (.*) (.*)", instr)
-        try:
-            val = int(val)
-        except:
-            val = registers[val]
-        if val != 0:
-            idx += int(jump_len) - 1
-
+    idx, registers = apply_instruction(idx, registers, instructions)
 print(f"Part 2 solution: {registers['a']}")
 
-
-#print(f"Part 1 solution: {part2_solution}")
 
 if __name__ == "__main__":
     pass
