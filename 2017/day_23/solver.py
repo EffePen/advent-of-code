115c16a56dd7243ed9bcf75c44ec588e65532dff
EffePi <francesco.penone@gmail.com>
2024-06-29 11:11:56 +0200

    Add day23 pt2 solution, AoC 2017

--- 2017/day_23/solver.py
+++ 2017/day_23/solver.py
@@ -6,6 +6,8 @@ import collections
 def parse_input():
     with open("input.txt") as f:
         instructions = f.read().splitlines()
+    # remove inline comments
+    instructions = [instr.split("#")[0].strip() for instr in instructions]
     return instructions
 
 
@@ -59,6 +61,58 @@ def execute_p1(instructions, registers, debug=False):
     return instr_count
 
 
+def execute_p2_expanded():
+    b = 57 * 100 + 100_000
+    c = b + 17000
+    h = 0
+    while True:
+        f = 1
+        d = 2
+        while True:
+            e = 2
+            while True:
+                if d * e == b:
+                    f = 0
+                e += 1
+                if e == b: break
+            d += 1
+            if d == b: break
+        if f == 0:
+            h += 1
+        if b == c:
+            break
+        b += 17
+    return h
+
+
+def is_prime(n):
+    return n > 1 and all(n % i for i in range(2, int(n ** 0.5) + 1))
+
+
+def execute_p2_optimized():
+    b = 57 * 100 + 100_000
+    c = b + 17000
+    if (c - b) % 17 != 0:
+        assert False
+    outer_loop_steps = (c - b) // 17 + 1
+    h = 0
+    for b_idx in range(outer_loop_steps):
+        b += 17
+        # the following code translates to "if b is not prime => f = 0"
+        # => "if b is not prime => h += 1"
+        # f = 1
+        # for d in range(2, b+1):
+        #     for e in range(2, b+1):
+        #         if d * e == b:
+        #             f = 0
+        # if f == 0:
+        #     h += 1
+        if not is_prime(b):
+            h += 1
+
+    return h
+
+
 instructions = parse_input()
 
 
@@ -67,11 +121,11 @@ registers = collections.defaultdict(lambda: 0)
 instr_count = execute_p1(instructions, registers)
 print("Part 1 solution: ", instr_count["mul"])
 
+
 # ######## PART 2
-registers = collections.defaultdict(lambda: 0)
-registers["a"] = 1
-gen_1_counter = execute_p1(instructions, registers)
-print("Part 2 solution: ", gen_1_counter)
+score = execute_p2_optimized()
+print("Part 1 solution: ", score)
+
 
 
 if __name__ == "__main__":
