eb0a92c770db621b4f90f227acbf8b1e9928f67d
EffePi <francesco.penone@gmail.com>
2024-12-15 13:18:50 +0100

    Unified day15 pt1 & pt2, AoC 2024

--- 2024/day_15/solver.py
+++ 2024/day_15/solver.py
@@ -8,11 +8,8 @@ def parse_input(part):
     if part == 2:
         grid_txt = grid_txt.replace("#", "##").replace(".", "..").replace("O", "[]").replace("@", "@.")
 
-    grid = {}
-    for r_idx, row in enumerate(grid_txt.splitlines()):
-        for c_idx, c in enumerate(row):
-            if c != ".":
-                grid[c_idx + 1j*r_idx] = c
+    grid = {c_idx + 1j*r_idx: c for r_idx, row in enumerate(grid_txt.splitlines())
+                                for c_idx, c in enumerate(row) if c != "."}
 
     moves_map = {"^": -1j, "<": -1, "v": 1j, ">": 1}
     moves = [moves_map[m] for m in moves_txt.replace("\n", "")]
@@ -52,7 +49,7 @@ def solve_pt1(grid, moves):
     return score
 
 
-def solve_pt2(grid, moves):
+def solve(grid, moves):
     curr_pos, = [p for p in grid if grid[p] == "@"]
     del grid[curr_pos]
 
@@ -67,10 +64,10 @@ def solve_pt2(grid, moves):
             if next_object == "#": # wall: don't move anything
                 blocked = True
                 break
-            elif next_object in ("[", "]"): # box: keep track of box positions
-                new_positions = {next_p, next_p + 1 if grid.get(next_p) == "[" else next_p - 1} - movable_box_positions
-                curr_positions.update(new_positions)
-                movable_box_positions.update(new_positions)
+            elif next_object in ("[", "]", "O"): # box: add positions to move
+                new_positions = {next_p} | (set() if next_object == "O" else {next_p + 1 if next_object == "[" else next_p - 1})
+                curr_positions.update(new_positions - movable_box_positions)
+                movable_box_positions.update(new_positions - movable_box_positions)
 
         if not blocked: # not blocked: update all movable box positions
             grid_patch = {p + m: grid[p] for p in movable_box_positions}
@@ -79,20 +76,20 @@ def solve_pt2(grid, moves):
             grid.update(grid_patch)
             curr_pos += m
 
-    score = sum(100*p.imag + p.real for p in grid if grid[p] == "[")
+    score = sum(100*p.imag + p.real for p in grid if grid[p] in ("[", "O"))
 
     return score
 
 
 # PART 1
 grid, moves = parse_input(part=1)
-score = solve_pt1(grid, moves)
+score = solve(grid, moves)
 print(f"Part 1 solution: {score}")
 
 
 # PART 2
 grid, moves = parse_input(part=2)
-score = solve_pt2(grid, moves)
+score = solve(grid, moves)
 print(f"Part 2 solution: {score}")
 
 
