a6bd68cf5878a48c257f0829aa74b84fba852c42
EffePi <francesco.penone@gmail.com>
2024-12-19 11:07:08 +0100

    Add day16 pt2, AoC 2024

--- 2024/day_16/solver.py
+++ 2024/day_16/solver.py
@@ -1,6 +1,5 @@
 
 import math
-from collections import defaultdict
 
 
 def parse_input():
@@ -11,41 +10,46 @@ def parse_input():
     return grid
 
 
-def solve_pt1(grid):
-    visited = dict()
+def solve(grid, part):
     start, = [p for p in grid if grid[p] == "S"]
     end, = [p for p in grid if grid[p] == "E"]
-    dir = +1
-    curr_statuses = {(start, dir, 0)}
-    end_values = list()
 
+    # find distances of (position, direction) with djikstra
+    visited = dict()
+    curr_statuses = {(start, +1, 0)} # (position, direction, distance)
     while curr_statuses:
         p, d, v = curr_statuses.pop()
         if visited.get((p, d), math.inf) < v or p not in grid:
             continue
-        if p == end:
-            end_values.append(v)
         visited[(p, d)] = v
         curr_statuses.update([(p+d, d, v+1), (p+d*1j, d*1j, v+1001), (p+d*-1j, d*-1j, v+1001), (p-d, -d, v+2001)])
-    score = min(end_values)
-    return score
+    best_score = min([v for (p, _), v in visited.items() if p == end])
 
-
-def solve_pt2(grid, best_val):
-    score = 0
-    return score
+    if part == 1:
+        return best_score
+    else: # reverse path and check compatible (position, direction) distances seen before
+        tiles = {end}
+        curr_statuses = set([(p, d, v) for (p, d), v in visited.items() if p == end and v == best_score])
+        while curr_statuses:
+            p, d, v = curr_statuses.pop()
+            p -= d
+            candidate_next_statues = [(p, d, v-1), (p, d*-1j, v-1001), (p, d*1j, v-1001), (p, -d, v-2001)]
+            candidate_next_statues = [cns for cns in candidate_next_statues if cns[2] == visited.get((cns[0], cns[1]), math.inf)]
+            tiles.update([cns[0] for cns in candidate_next_statues])
+            curr_statuses.update(candidate_next_statues)
+        return len(tiles)
 
 
 # PARSE INPUT
 grid = parse_input()
 
 # PART 1
-score = solve_pt1(grid)
+score = solve(grid, part=1)
 print(f"Part 1 solution: {score}")
 
 
 # PART 2
-score = solve_pt2(grid)
+score = solve(grid, part=2)
 print(f"Part 2 solution: {score}")
 
 
