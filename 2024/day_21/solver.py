8a353edbbc1324021143a81859d21512017d976f
EffePi <francesco.penone@gmail.com>
2024-12-21 19:14:13 +0100

    Opt day21 pt1: pre-calculated base transformations, AoC 2024

--- 2024/day_21/solver.py
+++ 2024/day_21/solver.py
@@ -1,6 +1,7 @@
 
 
 import re
+from functools import cache
 from collections import defaultdict
 
 
@@ -11,45 +12,45 @@ def parse_input():
     codes = input_txt.splitlines()
 
     # numpad grid
-    pad = defaultdict(dict)
-    pad["numpad"].update({c_idx + 0j: num for c_idx, num in enumerate(["7", "8", "9"])})
-    pad["numpad"].update({c_idx + 1j: num for c_idx, num in enumerate(["4", "5", "6"])})
-    pad["numpad"].update({c_idx + 2j: num for c_idx, num in enumerate(["1", "2", "3"])})
-    pad["numpad"].update({c_idx + 3j: num for c_idx, num in enumerate(["0", "A"], start=1)})
+    key2pos = defaultdict(dict)
+    key2pos["numpad"].update({num: c_idx + 0j for c_idx, num in enumerate(["7", "8", "9"])})
+    key2pos["numpad"].update({num: c_idx + 1j for c_idx, num in enumerate(["4", "5", "6"])})
+    key2pos["numpad"].update({num: c_idx + 2j for c_idx, num in enumerate(["1", "2", "3"])})
+    key2pos["numpad"].update({num: c_idx + 3j for c_idx, num in enumerate(["0", "A"], start=1)})
 
     # dirpad grid
-    pad["dirpad"].update({c_idx + 0j: num for c_idx, num in enumerate(["^", "A"], start=1)})
-    pad["dirpad"].update({c_idx + 1j: num for c_idx, num in enumerate(["<", "v", ">"])})
+    key2pos["dirpad"].update({num: c_idx + 0j for c_idx, num in enumerate(["^", "A"], start=1)})
+    key2pos["dirpad"].update({num: c_idx + 1j for c_idx, num in enumerate(["<", "v", ">"])})
 
-    return codes, pad
+    # pre-calculate transforms
+    base_transform = {}
+    for pad_type in key2pos:
+        base_transform[pad_type] = {}
+        for k1, k1_pos in key2pos[pad_type].items():
+            for k2, k2_pos in key2pos[pad_type].items():
+                dist = k2_pos - k1_pos
+                # NOTE: there is a bug. k2 should be k1
+                if key2pos[pad_type][k2] + dist.real in key2pos[pad_type].values():
+                    code = int(abs(dist.real)) * ("<" if dist.real < 0 else ">") + int(abs(dist.imag)) * ("^" if dist.imag < 0 else "v") + "A"
+                else:
+                    code = int(abs(dist.imag)) * ("^" if dist.imag < 0 else "v") + int(abs(dist.real)) * ("<" if dist.real < 0 else ">") + "A"
+                base_transform[pad_type][k1+k2] = code
+    return codes, base_transform
 
 
-def solve_pt1(codes, pad):
-    dir_map = {"^": -1j, "<": -1, "v": 1j, ">": 1}
-
-    rev_pad = {pad_type: {v:k for k, v in pad_dict.items()} for pad_type, pad_dict in pad.items()}
-
-    distances = {}
-    for pad_type in pad:
-        distances[pad_type] = {(k1, k2): k2_pos - k1_pos
-                               for k1_pos, k1 in pad[pad_type].items()
-                               for k2_pos, k2 in pad[pad_type].items()}
-
+def solve_pt1(codes, base_transform):
     score = 0
     for code in codes:
         numeric_part = int("".join(re.findall(r"\d", code)))
-        for pad_type in ["numpad"] + ["dirpad"] * 2:
-            prev_c = "A"
-            new_code = ""
-            for curr_c in code:
-                dist = distances[pad_type][(prev_c, curr_c)]
-                # check if intermediate position is valid
-                if rev_pad[pad_type][curr_c] + dist.real in pad[pad_type]:
-                    new_code += int(abs(dist.real)) * ("<" if dist.real < 0 else ">") + int(abs(dist.imag)) * ("^" if dist.imag < 0 else "v") + "A"
-                else:
-                    new_code += int(abs(dist.imag)) * ("^" if dist.imag < 0 else "v") + int(abs(dist.real)) * ("<" if dist.real < 0 else ">") + "A"
-                prev_c = curr_c
-            code = new_code
+        # from numpad to dirpad
+        new_codes = [base_transform["numpad"][prev_c + curr_c] for prev_c, curr_c in zip("A" + code[:-1], code)]
+
+        # iterate over robot dirpads
+        for idx in range(2):
+            code = "".join(new_codes)
+            new_codes = [base_transform["dirpad"][prev_c + curr_c] for prev_c, curr_c in zip("A" + code[:-1], code)]
+            code = "".join(new_codes)
+
         code_len = len(code)
         score += code_len * numeric_part
 
@@ -63,15 +64,15 @@ def solve_pt2(codes, pad):
 
 
 # PARSE INPUT
-codes, pad = parse_input()
+codes, base_transform = parse_input()
 
 # PART 1
-score = solve_pt1(codes, pad)
+score = solve_pt1(codes, base_transform)
 print(f"Part 1 solution: {score}")
 
 
 # PART 2
-score = solve_pt2(codes, pad)
+score = solve_pt2(codes, base_transform)
 print(f"Part 2 solution: {score}")
 
 
