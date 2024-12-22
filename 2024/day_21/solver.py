de75f5119d2bc46dd9515f95ec6368fea7f96854
EffePi <francesco.penone@gmail.com>
2024-12-22 23:45:37 +0100

    Add day21 pt2 and unified with pt1, AoC 2024

--- 2024/day_21/solver.py
+++ 2024/day_21/solver.py
@@ -29,53 +29,56 @@ def parse_input():
         for k1, k1_pos in key2pos[pad_type].items():
             for k2, k2_pos in key2pos[pad_type].items():
                 dist = k2_pos - k1_pos
-                # NOTE: there is a bug. k2 should be k1
-                if key2pos[pad_type][k2] + dist.real in key2pos[pad_type].values():
-                    code = int(abs(dist.real)) * ("<" if dist.real < 0 else ">") + int(abs(dist.imag)) * ("^" if dist.imag < 0 else "v") + "A"
-                else:
-                    code = int(abs(dist.imag)) * ("^" if dist.imag < 0 else "v") + int(abs(dist.real)) * ("<" if dist.real < 0 else ">") + "A"
-                base_transform[pad_type][k1+k2] = code
+                tmp_codes = set()
+                if key2pos[pad_type][k1] + dist.real in key2pos[pad_type].values():
+                    tmp_codes.add(int(abs(dist.real)) * ("<" if dist.real < 0 else ">") + int(abs(dist.imag)) * ("^" if dist.imag < 0 else "v") + "A")
+                if key2pos[pad_type][k1] + 1j * dist.imag in key2pos[pad_type].values():
+                    tmp_codes.add(int(abs(dist.imag)) * ("^" if dist.imag < 0 else "v") + int(abs(dist.real)) * ("<" if dist.real < 0 else ">") + "A")
+                base_transform[pad_type][k1+k2] = tmp_codes
     return codes, base_transform
 
 
-def solve_pt1(codes, base_transform):
+@cache
+def recurse(codes_counter, num_iter):
     score = 0
-    for code in codes:
-        numeric_part = int("".join(re.findall(r"\d", code)))
-        # from numpad to dirpad
-        new_codes = [base_transform["numpad"][prev_c + curr_c] for prev_c, curr_c in zip("A" + code[:-1], code)]
+    for code, cnt in codes_counter:
+        new_code_lists = [[]]
+        for prev_c, curr_c in zip("A" + code[:-1], code):
+            tmp_new_codes = []
+            for par_path in base_transform["dirpad"][prev_c + curr_c]:
+                tmp_new_codes += [nc + [par_path] for nc in new_code_lists]
+            new_code_lists = tmp_new_codes
+        # calculate counters and increase count "cnt" times
+        new_codes_counters = [Counter(ncl) for ncl in new_code_lists]
+        new_codes_counters = [{new_code: new_cnt * cnt for new_code, new_cnt in c.items()} for c in new_codes_counters]
 
-        # iterate over robot dirpads
-        for idx in range(14):
-            code = "".join(new_codes)
-            new_codes = [base_transform["dirpad"][prev_c + curr_c] for prev_c, curr_c in zip("A" + code[:-1], code)]
-            code = "".join(new_codes)
-
-        code_len = len(code)
-        score += code_len * numeric_part
-
+        if num_iter == 1:
+            score += min(sum(len(new_code) * new_cnt for new_code, new_cnt in cc.items()) for cc in new_codes_counters)
+        else:
+            score += min(recurse(tuple(cc.items()), num_iter=num_iter-1) for cc in new_codes_counters)
     return score
 
 
-def solve_pt2(codes, base_transform):
+def solve(codes, base_transform, num_iter):
     score = 0
     for code in codes:
         numeric_part = int("".join(re.findall(r"\d", code)))
+
         # from numpad to dirpad
-        new_codes = [base_transform["numpad"][prev_c + curr_c] for prev_c, curr_c in zip("A" + code[:-1], code)]
+        new_code_lists = [[]]
+        for prev_c, curr_c in zip("A" + code[:-1], code):
+            tmp_new_codes = []
+            for par_path in base_transform["numpad"][prev_c + curr_c]:
+                tmp_new_codes += [nc + [par_path] for nc in new_code_lists]
+            new_code_lists = tmp_new_codes
+        codes_counters = [Counter(ncl) for ncl in new_code_lists]
 
-        # iterate over robot dirpads
-        codes_counter = Counter(new_codes)
-        for idx in range(25):
-            new_codes_counter = defaultdict(int)
-            for new_code, cnt in codes_counter.items():
-                tmp_new_codes = [base_transform["dirpad"][prev_c + curr_c] for prev_c, curr_c in zip("A" + new_code[:-1], new_code)]
-                for tmp_new_code in tmp_new_codes:
-                    new_codes_counter[tmp_new_code] += cnt
-            codes_counter = new_codes_counter
-
-        code_len = sum(len(code) * cnt for code, cnt in codes_counter.items())
-        score += code_len * numeric_part
+        # iterate over dirpads recursively
+        tmp_scores = []
+        for code_counter in codes_counters:
+            code_len = recurse(tuple(code_counter.items()), num_iter=num_iter)
+            tmp_scores.append(code_len * numeric_part)
+        score += min(tmp_scores)
 
     return score
 
@@ -84,13 +87,14 @@ def solve_pt2(codes, base_transform):
 # PARSE INPUT
 codes, base_transform = parse_input()
 
+
 # PART 1
-score = solve_pt1(codes, base_transform)
+score = solve(codes, base_transform, num_iter=2)
 print(f"Part 1 solution: {score}")
 
 
 # PART 2
-score = solve_pt2(codes, base_transform)
+score = solve(codes, base_transform, num_iter=25)
 print(f"Part 2 solution: {score}")
 
 
