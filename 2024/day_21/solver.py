d48242dc2f893ecb5bab1e8665855de4040b8689
EffePi <francesco.penone@gmail.com>
2024-12-23 01:05:51 +0100

    Simplified and commented day21 code, AoC 2024

--- 2024/day_21/solver.py
+++ 2024/day_21/solver.py
@@ -23,6 +23,8 @@ def parse_input():
     key2pos["dirpad"].update({num: c_idx + 1j for c_idx, num in enumerate(["<", "v", ">"])})
 
     # pre-calculate transforms
+    # consider only straight paths (horizontal first, then vertical and vice-versa)
+    # since the mixed horizontal-vertical sequences would lead to longer sequences
     base_transform = {}
     for pad_type in key2pos:
         base_transform[pad_type] = {}
@@ -38,50 +40,50 @@ def parse_input():
     return codes, base_transform
 
 
-@cache
-def recurse(codes_counter, num_iter):
-    score = 0
-    for code, cnt in codes_counter:
-        new_code_lists = [[]]
-        for prev_c, curr_c in zip("A" + code[:-1], code):
-            tmp_new_codes = []
-            for par_path in base_transform["dirpad"][prev_c + curr_c]:
-                tmp_new_codes += [nc + [par_path] for nc in new_code_lists]
-            new_code_lists = tmp_new_codes
-        # calculate counters and increase count "cnt" times
-        new_codes_counters = [Counter(ncl) for ncl in new_code_lists]
-        new_codes_counters = [{new_code: new_cnt * cnt for new_code, new_cnt in c.items()} for c in new_codes_counters]
+def get_recurse_fn(base_transform):
+    @cache
+    def recurse(codes_counter, pads):
+        score = 0
+        curr_pad, next_pads = pads[0], pads[1:]
 
-        if num_iter == 1:
-            score += min(sum(len(new_code) * new_cnt for new_code, new_cnt in cc.items()) for cc in new_codes_counters)
-        else:
-            score += min(recurse(tuple(cc.items()), num_iter=num_iter-1) for cc in new_codes_counters)
-    return score
+        # NOTE: since each sub-code ends with "A" => each sub-code is preceded by "A" => they can evolve independently
+        for code, cnt in codes_counter:
+            # prepare all alternative new code lists: fork when multiple base transformations for the same char pairs
+            new_code_lists = [[]]
+            for idx in range(len(code)):
+                tmp_new_codes = []
+                for par_path in base_transform[curr_pad][("A" + code)[idx:idx+2]]:
+                    tmp_new_codes += [nc + [par_path] for nc in new_code_lists]
+                new_code_lists = tmp_new_codes
+
+            # calculate new counters and increase count "cnt" times
+            new_codes_counters = [Counter(ncl) for ncl in new_code_lists]
+            new_codes_counters = [{new_code: new_cnt * cnt for new_code, new_cnt in c.items()} for c in new_codes_counters]
+
+            # keep only the shortest sequence
+            if len(pads) == 1:
+                score += min(sum(len(new_code) * new_cnt for new_code, new_cnt in cc.items()) for cc in new_codes_counters)
+            else:
+                score += min(recurse(tuple(cc.items()), pads=next_pads) for cc in new_codes_counters)
+        return score
+
+    return recurse
 
 
 def solve(codes, base_transform, num_iter):
+    # prepare recurse function
+    recurse_fn = get_recurse_fn(base_transform)
+
     score = 0
     for code in codes:
+        # extract numeric part from initial code
         numeric_part = int("".join(re.findall(r"\d", code)))
 
-        # from numpad to dirpad
-        new_code_lists = [[]]
-        for prev_c, curr_c in zip("A" + code[:-1], code):
-            tmp_new_codes = []
-            for par_path in base_transform["numpad"][prev_c + curr_c]:
-                tmp_new_codes += [nc + [par_path] for nc in new_code_lists]
-            new_code_lists = tmp_new_codes
-        codes_counters = [Counter(ncl) for ncl in new_code_lists]
-
-        # iterate over dirpads recursively
-        tmp_scores = []
-        for code_counter in codes_counters:
-            code_len = recurse(tuple(code_counter.items()), num_iter=num_iter)
-            tmp_scores.append(code_len * numeric_part)
-        score += min(tmp_scores)
+        # iterate over pads recursively and get total length
+        code_len = recurse_fn(((code, 1),), pads=tuple(["numpad"] + num_iter * ["dirpad"]))
+        score += numeric_part * code_len
 
     return score
-
 
 
 # PARSE INPUT
